bc9fb2312aed46577965fd1164978dda195347cc
Robert Jamborski <jamborski.webdesign@gmail.com>
2021-04-29 21:55:49 +0100

    Updates

---
+++ precache-manifest.e8ddd6b84f264ddb43d7f4953a4b1a55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7653b2458f9fc01ede0fbc913e4bd33",
+    "url": "/react-express-todui-repro/index.html"
+  },
+  {
+    "revision": "519c6f4690e609246ccb",
+    "url": "/react-express-todui-repro/static/js/2.8403e8f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-express-todui-repro/static/js/2.8403e8f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b703f93788dd9f09eea",
+    "url": "/react-express-todui-repro/static/js/main.b63a07e3.chunk.js"
+  },
+  {
+    "revision": "76e2ec658c0fe2e1b592",
+    "url": "/react-express-todui-repro/static/js/runtime-main.51409d51.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jamborski-dev/react-express-todui-repro/precache-manifest.50513dfd2d5f4f1ff970bd3fecf35a38.js"
+  "/react-express-todui-repro/precache-manifest.e8ddd6b84f264ddb43d7f4953a4b1a55.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jamborski-dev/react-express-todui-repro/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-express-todui-repro/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
